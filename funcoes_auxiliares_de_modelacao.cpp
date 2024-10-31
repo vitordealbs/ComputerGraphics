982dad52f9b519e2c65c496e0efe443fe13abe46
luis-dot-lopes <luisantonio20054@gmail.com>
2024-10-31 15:25:57 -0300

    Novas funções auxiliares adicionadas

--- funcoes_auxiliares_de_modelacao.cpp
+++ funcoes_auxiliares_de_modelacao.cpp
@@ -89,6 +89,35 @@ Raio::intersecao(Esfera esfera)
   }
   return t;
 }
+
+float
+Raio::intersecao(Cilindro cilindro)
+{
+  Vetor3d v = P0 - cilindro.centro;
+  Vetor3d u = dr - dr.dot_product(cilindro.direcao) * cilindro.direcao;
+  Vetor3d w = v - (v.dot_product(cilindro.direcao)) * cilindro.direcao;
+  double a = u.dot_product(u);
+  double b = 2 * u.dot_product(w);
+  double c = w.dot_product(w) - cilindro.raio * cilindro.raio;
+  double delta = b * b - 4 * a * c;
+  if (delta < 0.0) {
+    return -1.0f;
+  }
+  double t = (-b - sqrt(delta)) / (2 * a);
+  if (t < 0.0) {
+    t = (-b + sqrt(delta)) / (2 * a);
+  }
+  if (no_ponto(t).dot_product(cilindro.direcao) > cilindro.altura) {
+    return -1.0f;
+  }
+  return t;
+}
+
+float
+Raio::intersecao(Cone cone)
+{
+}
+
 Plano::Plano(Vetor3d ponto,
              Vetor3d normal,
              Vetor3d K_d,
@@ -103,6 +132,23 @@ Plano::Plano(Vetor3d ponto,
   , m(m)
 {
 }
+
+Plano
+Plano::tres_pontos(Vetor3d P1,
+                   Vetor3d P2,
+                   Vetor3d P3,
+                   Vetor3d K_d,
+                   Vetor3d K_e,
+                   Vetor3d K_a,
+                   float m)
+{
+  Vetor3d v1 = P2 - P1;
+  Vetor3d v2 = P3 - P1;
+  Vetor3d w = v1.cross_product(v2);
+  Vetor3d n = w.normalizado();
+  return Plano(P1, n, K_d, K_e, K_a, m);
+}
+
 float
 Raio::intersecao(Plano plano)
 {
@@ -114,6 +160,7 @@ Raio::intersecao(Plano plano)
   double b = v.dot_product(plano.normal);
   return -b / a;
 }
+
 Vetor3d
 Plano::calcular_iluminacao(Vetor3d Pt,
                            Vetor3d dr,
@@ -121,6 +168,76 @@ Plano::calcular_iluminacao(Vetor3d Pt,
                            Vetor3d I_F,
                            Vetor3d I_A)
 {
+  Vetor3d v = dr * -1;
+  Vetor3d l = (P_F - Pt).normalizado();
+  float dotproduct_nl = normal.dot_product(l);
+  Vetor3d r = 2 * dotproduct_nl * normal - l;
+  float dotproduct_vr = v.dot_product(r);
+
+  Vetor3d I_a = K_a * I_A;
+  Vetor3d I_d = K_d * I_F * max(dotproduct_nl, 0.0);
+  Vetor3d I_e = K_e * I_F * pow(max(dotproduct_vr, 0.0), m);
+
+  Vetor3d I_total = I_d + I_e + I_a;
+  return I_total;
+}
+
+Cone::Cone(Vetor3d centro,
+           float raio,
+           float altura,
+           Vetor3d direcao,
+           Vetor3d K_d,
+           Vetor3d K_e,
+           Vetor3d K_a,
+           float m)
+  : centro(centro)
+  , raio(raio)
+  , altura(altura)
+  , direcao(direcao)
+  , K_d(K_d)
+  , K_e(K_e)
+  , K_a(K_a)
+  , m(m)
+{
+}
+
+Vetor3d
+Cone::calcular_iluminacao(Vetor3d Pt,
+                          Vetor3d dr,
+                          Vetor3d P_F,
+                          Vetor3d I_F,
+                          Vetor3d I_A)
+{
+}
+
+Cilindro::Cilindro(Vetor3d centro,
+                   float raio,
+                   float altura,
+                   Vetor3d direcao,
+                   Vetor3d K_d,
+                   Vetor3d K_e,
+                   Vetor3d K_a,
+                   float m)
+  : centro(centro)
+  , raio(raio)
+  , altura(altura)
+  , direcao(direcao)
+  , K_d(K_d)
+  , K_e(K_e)
+  , K_a(K_a)
+  , m(m)
+{
+}
+
+Vetor3d
+Cilindro::calcular_iluminacao(Vetor3d Pt,
+                              Vetor3d dr,
+                              Vetor3d P_F,
+                              Vetor3d I_F,
+                              Vetor3d I_A)
+{
+  Vetor3d z = direcao;
+  Vetor3d normal = (Pt - (Pt.dot_product(z)) * z).normalizado();
   Vetor3d v = dr * -1;
   Vetor3d l = (P_F - Pt).normalizado();
   float dotproduct_nl = normal.dot_product(l);
--- funcoes_auxiliares_de_modelacao.h
+++ funcoes_auxiliares_de_modelacao.h
@@ -6,49 +6,148 @@
 #include "funcoes_auxiliares.h" // Incluindo o arquivo de operações vetoriais
 using namespace funcoes_auxiliares;
 
-    namespace Auxiliares_de_modelacao {
+namespace Auxiliares_de_modelacao {
 
-    struct Plano; // Declaração antecipada de Plano
+struct Objeto
+{
+  virtual Vetor3d calcular_iluminacao(Vetor3d Pt,
+                                      Vetor3d dr,
+                                      Vetor3d P_F,
+                                      Vetor3d I_F,
+                                      Vetor3d I_A) = 0;
+};
 
-    struct Esfera {
-        Vetor3d centro;
-        float raio;
-        Vetor3d K_d;
-        Vetor3d K_e;
-        Vetor3d K_a;
-        float m;
+struct Plano; // Declaração antecipada de Plano
 
-        Esfera(Vetor3d centro, float raio, Vetor3d K_d, Vetor3d K_e, Vetor3d K_a, float m);
-        Vetor3d calcular_iluminacao(Vetor3d Pt, Vetor3d dr, Vetor3d P_F, Vetor3d I_F, Vetor3d I_A);
-    };
+struct Esfera : Objeto
+{
+  Vetor3d centro;
+  float raio;
+  Vetor3d K_d;
+  Vetor3d K_e;
+  Vetor3d K_a;
+  float m;
 
-        struct Plano {
-            Vetor3d ponto;
-            Vetor3d normal;
-            Vetor3d K_d;
-            Vetor3d K_e;
-            Vetor3d K_a;
-            float m;
+  Esfera(Vetor3d centro,
+         float raio,
+         Vetor3d K_d,
+         Vetor3d K_e,
+         Vetor3d K_a,
+         float m);
 
-            // Construtor correto para inicializar as propriedades do plano
-            Plano(Vetor3d ponto, Vetor3d normal, Vetor3d K_d, Vetor3d K_e, Vetor3d K_a, float m);
-            Vetor3d calcular_iluminacao(Vetor3d ponto, Vetor3d normal, Vetor3d K_d, Vetor3d K_e, Vetor3d K_a);
-        };
+  Vetor3d calcular_iluminacao(Vetor3d Pt,
+                              Vetor3d dr,
+                              Vetor3d P_F,
+                              Vetor3d I_F,
+                              Vetor3d I_A);
+};
 
+struct Plano : Objeto
+{
+  Vetor3d ponto;
+  Vetor3d normal;
+  Vetor3d K_d;
+  Vetor3d K_e;
+  Vetor3d K_a;
+  float m;
 
-    struct Raio {
-        Vetor3d P0;
-        Vetor3d dr;
+  // Construtor correto para inicializar as propriedades do plano
+  Plano(Vetor3d ponto,
+        Vetor3d normal,
+        Vetor3d K_d,
+        Vetor3d K_e,
+        Vetor3d K_a,
+        float m);
 
-        Raio(Vetor3d P0, Vetor3d dr);
-        Vetor3d no_ponto(float t);
-        float intersecao(Esfera esfera);
-        float intersecao(Plano plano); // Função de intersecção com Plano
-    };
+  static Plano tres_pontos(Vetor3d P1,
+                           Vetor3d P2,
+                           Vetor3d P3,
+                           Vetor3d K_d,
+                           Vetor3d K_e,
+                           Vetor3d K_a,
+                           float m);
 
-    // Funções max e min
-    double max(double a, double b);
-    double min(double a, double b);
+  Vetor3d calcular_iluminacao(Vetor3d Pt,
+                              Vetor3d dr,
+                              Vetor3d P_F,
+                              Vetor3d I_F,
+                              Vetor3d I_A);
+};
+
+struct Cilindro : Objeto
+{
+  Vetor3d centro;
+  float raio;
+  float altura;
+  Vetor3d direcao;
+
+  Vetor3d K_d;
+  Vetor3d K_e;
+  Vetor3d K_a;
+  float m;
+
+  Cilindro(Vetor3d centro,
+           float raio,
+           float altura,
+           Vetor3d direcao,
+           Vetor3d K_d,
+           Vetor3d K_e,
+           Vetor3d K_a,
+           float m);
+
+  Vetor3d calcular_iluminacao(Vetor3d Pt,
+                              Vetor3d dr,
+                              Vetor3d P_F,
+                              Vetor3d I_F,
+                              Vetor3d I_A);
+};
+
+struct Cone : Objeto
+{
+  Vetor3d centro;
+  float raio;
+  float altura;
+  Vetor3d direcao;
+
+  Vetor3d K_d;
+  Vetor3d K_e;
+  Vetor3d K_a;
+  float m;
+
+  Cone(Vetor3d centro,
+       float raio,
+       float altura,
+       Vetor3d direcao,
+       Vetor3d K_d,
+       Vetor3d K_e,
+       Vetor3d K_a,
+       float m);
+
+  Vetor3d calcular_iluminacao(Vetor3d Pt,
+                              Vetor3d dr,
+                              Vetor3d P_F,
+                              Vetor3d I_F,
+                              Vetor3d I_A);
+};
+
+struct Raio
+{
+  Vetor3d P0;
+  Vetor3d dr;
+
+  Raio(Vetor3d P0, Vetor3d dr);
+  Vetor3d no_ponto(float t);
+  float intersecao(Esfera esfera);
+  float intersecao(Plano plano); // Função de intersecção com Plano
+  float intersecao(Cone cone);
+  float intersecao(Cilindro cilindro);
+};
+
+// Funções max e min
+double
+max(double a, double b);
+double
+min(double a, double b);
 
 }
 
